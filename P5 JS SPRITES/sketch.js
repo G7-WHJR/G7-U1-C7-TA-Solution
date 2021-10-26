2ca525883f5f6e8f24091f690e98f467186d4d7b
unknown <raveen.anand@whitehatjr.com>
2021-10-26 13:51:49 +0530

    v2

--- P5 JS SPRITES/sketch.js
+++ P5 JS SPRITES/sketch.js
@@ -2,11 +2,9 @@ var box;
 function setup() {
   createCanvas(400, 400);
   box = createSprite(40, 40, 40, 40);
-  box.shapeColor = "white";
-  //horizontal
-  box.setVelocity(0.5,0);
-  //vertical
-  //box.setVelocity(0,0.5);
+  box.shapeColor = "white"; 
+  box.velocityX=0.5;//horizontal
+  //box.velocityY=0.5;//vertical
 }
 function draw() {
   background("orange");
