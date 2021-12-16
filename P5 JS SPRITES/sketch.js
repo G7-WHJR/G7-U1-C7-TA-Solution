48a9dbeef4d45f0ecb0235be6024a46a352de3b3
unknown <raveen.anand@whitehatjr.com>
2021-12-16 18:33:59 +0530

    v2

--- P5 JS SPRITES/sketch.js
+++ P5 JS SPRITES/sketch.js
@@ -3,7 +3,12 @@ function setup() {
   createCanvas(400, 400);
   box = createSprite(40, 40, 40, 40);
   box.shapeColor = "white"; 
+
+  //For teacher activity 1 , velocityX will be set to 0.5:
   box.velocityX=0.5;//horizontal
+  
+  //For student activity 1 , velocityY will be set to 0.5:
+  box.velocityY=0.5;//vertical
 }
 function draw() {
   background("orange");
